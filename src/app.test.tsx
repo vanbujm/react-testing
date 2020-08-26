b4b07bd8dfdb2104ec3d9f5c2346753e8c99c836
Jonathan Van Buren <vanbujm@gmail.com>
2020-08-26 18:49:47 +0800

    tweaked comments

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -3,6 +3,7 @@ import { render, getByTestId, fireEvent, getAllByRole, getByText } from '@testin
 import { Game } from './app';
 
 describe('App', () => {
+  // Please never name a test this
   it('renders 😎', () => {
     // Get me the game board
     const { container } = render(<Game />);
@@ -17,17 +18,19 @@ describe('App', () => {
   // So far so good...
 
   it('Places an "X" on the board when you click a square 🤔', async () => {
-    // Uuuuh guess i better start with the game board again...
+    // Uuuuh guess I better start with the game board again... Is this building an entire virtual dom for every test? 😭 (yes)
     const { container } = render(<Game />);
 
     // Get me a square?
     // const square = getByRole('button');
     // Shit, doesn't work got too many buttons
-    // Guess I am adding more test Ids...
+    // Guess I am adding more test ids...
     // const square = getByTestId(container, 'square');
     // Wait... That doesn't work either there are multiple squares...
+    // Guess I am going to spin the wheel of chance and hope no one puts a button higher up on the dom tree in future updates...
     const square = getAllByRole(container, 'button')[0];
 
+    // What if I have one handler handling click and mouse 'enter' events for accessibility/ease-of-use? Two tests? Two dom renders? 😱
     fireEvent.click(square);
 
     const x = getByText(container, 'X');
@@ -35,5 +38,5 @@ describe('App', () => {
     expect(x.outerHTML).toMatchInlineSnapshot(`"<button class=\\"square\\" data-testid=\\"square\\">X</button>"`);
   });
 
-  // Uuugh it took nearly an hour to figure out how to do that, I CBF doing any more tests for now.
+  // Uuugh it took nearly an hour to figure out how to do that, I CBF doing any more tests, time to go scream into a pillow.
 });
